acaf83aa467e145f9402f644f0c1e3462d27e849
Mick Koch <mick@kochm.co>
2015-10-11 09:57:31 -0400

    Add some docs to bits

--- src/p25/bits.rs
+++ src/p25/bits.rs
@@ -1,76 +1,101 @@
+//! This module defines the `Bits` and `Dibits` iterators as well as the wrapper types
+//! `Bit` and `Dibit`, for working with sub-byte values.
+//!
+//! The wrapped values of `Bit` and `Dibit` are guaranteed to have only one or two bits,
+//! respectively.
+
 use std;
 
+/// Iterate over individual bits of a byte source, MSB to LSB.
 pub type Bits<T> = SubByteIter<BitParams, T>;
+/// Iterate over the dibits of a byte source, MSB to LSB.
 pub type Dibits<T> = SubByteIter<DibitParams, T>;
 
-pub trait IterateParams {
+/// Defines parameters needed for (power of two) sub-byte iterators.
+pub trait IterParams {
+    /// Type to yield at each iteration.
     type IterType;
 
+    /// Number of bits to consume at each iteration.
     fn bits() -> u8;
-    fn wrap(x: u8) -> Self::IterType;
+    /// Wrap the given bits in container type.
+    fn wrap(bits: u8) -> Self::IterType;
 
+    /// Number of iterations needed for each byte.
     fn iterations() -> u8 { 8 / Self::bits() }
 
+    /// Verify the parameters are supported.
     fn validate() {
+        // Only powers of two are valid because there can be no "leftovers."
         assert!(Self::bits().is_power_of_two());
     }
 }
 
+/// A single bit.
 pub struct Bit(u8);
 
 impl Bit {
+    /// Construct a new `Bit` with the given bit in the LSB position.
     pub fn new(bits: u8) -> Bit {
         assert!(bits & 0b11111110 == 0);
         Bit(bits)
     }
 
-    pub fn bits(&self) -> u8 { self.0 }
+    /// Get the wrapped bit value.
+    pub fn bit(&self) -> u8 { self.0 }
 }
 
+/// Parameters for `Bits` iterator.
 pub struct BitParams;
 
-impl IterateParams for BitParams {
+impl IterParams for BitParams {
     type IterType = Bit;
     fn bits() -> u8 { 1 }
-    fn wrap(x: u8) -> Bit { Bit::new(x) }
+    fn wrap(bits: u8) -> Bit { Bit::new(bits) }
 }
 
 #[derive(Debug)]
+/// Two bits.
 pub struct Dibit(u8);
 
 impl Dibit {
+    /// Construct a new `Dibit` with the two given bits in the LSB position.
     pub fn new(bits: u8) -> Dibit {
         assert!(bits & 0b11111100 == 0);
         Dibit(bits)
     }
 
+    /// Get the wrapped dibit.
     pub fn bits(&self) -> u8 { self.0 }
 }
 
+/// Parameters for `Dibits` iterator.
 pub struct DibitParams;
 
-impl IterateParams for DibitParams {
+impl IterParams for DibitParams {
     type IterType = Dibit;
     fn bits() -> u8 { 2 }
-    fn wrap(x: u8) -> Dibit { Dibit::new(x) }
+    fn wrap(bits: u8) -> Dibit { Dibit::new(bits) }
 }
 
-pub struct SubByteIter<P, T>
-    where P: IterateParams, T: Iterator<Item = u8>
+/// An iterator for sub-byte (bit-level) values.
+pub struct SubByteIter<P, T> where
+    P: IterParams, T: Iterator<Item = u8>
 {
     params: std::marker::PhantomData<P>,
-    /// The source of bits.
+    /// Source of bytes.
     src: T,
-    /// The current index into the current byte.
+    /// Current bit-level index into the current byte.
     idx: u8,
-    /// The current byte in the source.
+    /// Current byte in the source.
     byte: u8,
 }
 
-impl<P, T> SubByteIter<P, T>
-    where P: IterateParams, T: Iterator<Item = u8>
+impl<P, T> SubByteIter<P, T> where
+    P: IterParams, T: Iterator<Item = u8>
 {
-    /// Construct a new `SubByteIter<T>`.
+    /// Construct a new `SubByteIter` over the given byte source. All bits are iterated
+    /// over, so the number of bits must be a byte multiple.
     pub fn new(src: T) -> SubByteIter<P, T> {
         SubByteIter {
             params: std::marker::PhantomData,
@@ -81,8 +106,8 @@ impl<P, T> SubByteIter<P, T>
     }
 }
 
-impl<P, T> Iterator for SubByteIter<P, T>
-    where P: IterateParams, T: Iterator<Item = u8>
+impl<P, T> Iterator for SubByteIter<P, T> where
+    P: IterParams, T: Iterator<Item = u8>
 {
     type Item = P::IterType;
 
@@ -94,8 +119,8 @@ impl<P, T> Iterator for SubByteIter<P, T>
             };
         }
 
-        // Store the current byte for later.
-        let byte = self.byte;
+        // Extract MSBs.
+        let bits = self.byte >> (8 - P::bits());
 
         // Strip off the MSBs for the next iteration.
         self.byte <<= P::bits();
@@ -104,8 +129,7 @@ impl<P, T> Iterator for SubByteIter<P, T>
         self.idx += 1;
         self.idx %= P::iterations();
 
-        // Yield the MSBs.
-        Some(P::wrap(byte >> (8 - P::bits())))
+        Some(P::wrap(bits))
     }
 }
 
