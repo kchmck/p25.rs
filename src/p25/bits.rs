5d25d47b1d044ab1d38ee461b6d659dd2760f9a8
Mick Koch <mick@kochm.co>
2015-10-11 11:21:21 -0400

    Add Tribit and Tribits

--- src/p25/bits.rs
+++ src/p25/bits.rs
@@ -1,8 +1,8 @@
-//! This module defines the `Bits` and `Dibits` iterators as well as the wrapper types
-//! `Bit` and `Dibit`, for working with sub-byte values.
+//! This module defines the `Bits`, `Dibits`, and `Tribits` iterators as well as the
+//! wrapper types `Bit`, `Dibit`, and `Tribit`, for working with sub-byte values.
 //!
-//! The wrapped values of `Bit` and `Dibit` are guaranteed to have only one or two bits,
-//! respectively.
+//! The wrapped values of `Bit`, `Dibit`, and `Tribit, are guaranteed to have only one,
+//! two, or three bits, respectively.
 
 use std;
 
@@ -79,6 +79,21 @@ impl IterParams for DibitParams {
     fn wrap(bits: u8) -> Dibit { Dibit::new(bits) }
 }
 
+/// Three bits.
+#[derive(Copy, Clone, Default, Debug, PartialEq, Eq)]
+pub struct Tribit(u8);
+
+impl Tribit {
+    /// Construct a new `Tribit` with the three given bits in the LSB position.
+    pub fn new(bits: u8) -> Tribit {
+        assert!(bits & 0b11111000 == 0);
+        Tribit(bits)
+    }
+
+    /// Get the wrapped tribit.
+    pub fn bits(&self) -> u8 { self.0 }
+}
+
 /// An iterator for sub-byte (bit-level) values.
 pub struct SubByteIter<P, T> where
     P: IterParams, T: Iterator<Item = u8>
@@ -134,6 +149,80 @@ impl<P, T> Iterator for SubByteIter<P, T> where
     }
 }
 
+/// Iterates over the tribits in a byte source.
+pub struct Tribits<T: Iterator<Item = u8>> {
+    /// The source of bytes.
+    src: T,
+    /// Current bit buffer, containing either 6 or 3 bits (in MSB position) or 0.
+    bits: u8,
+    /// Tribit index into `bits` (0 or 1).
+    idx: usize,
+    /// Buffered bits from current source byte, to be added to `bits`.
+    buf: u8,
+    /// Number of bits in `buf`, either 2, 4, 6, or 0.
+    buf_bits: usize,
+}
+
+impl<T: Iterator<Item = u8>> Tribits<T> {
+    /// Construct a new `Tribits` from the given source of bytes. The number of bytes must
+    /// be a multiple of 3 (a multiple of 24 bits).
+    pub fn new(src: T) -> Tribits<T> {
+        Tribits {
+            src: src,
+            bits: 0,
+            idx: 0,
+            buf: 0,
+            buf_bits: 0,
+        }
+    }
+}
+
+impl<T: Iterator<Item = u8>> Iterator for Tribits<T> {
+    type Item = Tribit;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // If on the first tribit, it's time to flush the buffer and (maybe) load another
+        // byte.
+        if self.idx == 0 {
+            // Flush and reset the buffer.
+            self.bits = self.buf;
+            self.buf = 0;
+
+            // Calculate the number of bits to buffer for the next iteration.
+            self.buf_bits += 2;
+            self.buf_bits %= 8;
+
+            // Only load a new byte if bits need to be buffered.
+            if self.buf_bits != 0 {
+                let next = match self.src.next() {
+                    Some(b) => b,
+                    None => if self.buf_bits == 2 {
+                        // In this case we've covered 8 tribits = 24 bits = 3 bytes
+                        // exactly, so it's fine if there are no more bytes.
+                        return None;
+                    } else {
+                        panic!("incomplete tribit");
+                    }
+                };
+
+                // Add in some source bits after the MSBs.
+                self.bits |= next >> self.buf_bits << 2;
+                // Buffer the rest of the bits.
+                self.buf = next << (8 - self.buf_bits);
+            }
+        }
+
+        // Extract the 3 MSBs and strip them off for next time.
+        let bits = self.bits >> 5;
+        self.bits <<= 3;
+
+        self.idx += 1;
+        self.idx %= 2;
+
+        Some(Tribit::new(bits))
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -181,5 +270,45 @@ mod test {
             assert!(b.next().unwrap().0 == 0);
             assert!(b.next().unwrap().0 == 0);
         }
+    }
+
+    #[test]
+    fn test_tribits() {
+        let bytes = [
+            0b00101001,
+            0b11001011,
+            0b10111000,
+            0b00101001,
+            0b11001011,
+            0b10111000,
+        ];
+        let mut t = Tribits::new(bytes.iter().cloned());
+
+        assert_eq!(t.next().unwrap().bits(), 0b001);
+        assert_eq!(t.next().unwrap().bits(), 0b010);
+        assert_eq!(t.next().unwrap().bits(), 0b011);
+        assert_eq!(t.next().unwrap().bits(), 0b100);
+        assert_eq!(t.next().unwrap().bits(), 0b101);
+        assert_eq!(t.next().unwrap().bits(), 0b110);
+        assert_eq!(t.next().unwrap().bits(), 0b111);
+        assert_eq!(t.next().unwrap().bits(), 0b000);
+        assert_eq!(t.next().unwrap().bits(), 0b001);
+        assert_eq!(t.next().unwrap().bits(), 0b010);
+        assert_eq!(t.next().unwrap().bits(), 0b011);
+        assert_eq!(t.next().unwrap().bits(), 0b100);
+        assert_eq!(t.next().unwrap().bits(), 0b101);
+        assert_eq!(t.next().unwrap().bits(), 0b110);
+        assert_eq!(t.next().unwrap().bits(), 0b111);
+        assert_eq!(t.next().unwrap().bits(), 0b000);
+        assert!(t.next().is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_tribits_panic() {
+        let bytes = [1, 2, 3, 4];
+        let t = Tribits::new(bytes.iter().cloned());
+
+        for _ in t {}
     }
 }
