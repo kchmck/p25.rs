ad8df52b6528c85479f4ded4718e01ac9c4b715f
Mick Koch <mick@kochm.co>
2015-10-25 19:59:36 -0400

    Bit-reverse all codewords, for consistency with P25

--- src/p25/galois.rs
+++ src/p25/galois.rs
@@ -9,68 +9,68 @@ impl GaloisField for P25Field {
 
     fn codeword(pow: usize) -> u8 {
         const CODEWORDS: &'static [u8] = &[
-            0b100000,
-            0b010000,
-            0b001000,
-            0b000100,
-            0b000010,
             0b000001,
-            0b110000,
-            0b011000,
-            0b001100,
-            0b000110,
+            0b000010,
+            0b000100,
+            0b001000,
+            0b010000,
+            0b100000,
             0b000011,
-            0b110001,
-            0b101000,
-            0b010100,
-            0b001010,
-            0b000101,
-            0b110010,
-            0b011001,
-            0b111100,
-            0b011110,
-            0b001111,
-            0b110111,
-            0b101011,
-            0b100101,
-            0b100010,
-            0b010001,
-            0b111000,
-            0b011100,
-            0b001110,
-            0b000111,
-            0b110011,
-            0b101001,
-            0b100100,
-            0b010010,
-            0b001001,
-            0b110100,
-            0b011010,
-            0b001101,
-            0b110110,
-            0b011011,
-            0b111101,
-            0b101110,
-            0b010111,
-            0b111011,
-            0b101101,
-            0b100110,
-            0b010011,
-            0b111001,
-            0b101100,
-            0b010110,
-            0b001011,
-            0b110101,
-            0b101010,
-            0b010101,
-            0b111010,
-            0b011101,
-            0b111110,
-            0b011111,
-            0b111111,
-            0b101111,
-            0b100111,
+            0b000110,
+            0b001100,
+            0b011000,
+            0b110000,
             0b100011,
+            0b000101,
+            0b001010,
+            0b010100,
+            0b101000,
+            0b010011,
+            0b100110,
+            0b001111,
+            0b011110,
+            0b111100,
+            0b111011,
+            0b110101,
+            0b101001,
+            0b010001,
+            0b100010,
+            0b000111,
+            0b001110,
+            0b011100,
+            0b111000,
+            0b110011,
+            0b100101,
+            0b001001,
+            0b010010,
+            0b100100,
+            0b001011,
+            0b010110,
+            0b101100,
+            0b011011,
+            0b110110,
+            0b101111,
+            0b011101,
+            0b111010,
+            0b110111,
+            0b101101,
+            0b011001,
+            0b110010,
+            0b100111,
+            0b001101,
+            0b011010,
+            0b110100,
+            0b101011,
+            0b010101,
+            0b101010,
+            0b010111,
+            0b101110,
+            0b011111,
+            0b111110,
+            0b111111,
+            0b111101,
+            0b111001,
+            0b110001,
             0b100001,
         ];
 
@@ -79,68 +79,68 @@ impl GaloisField for P25Field {
 
     fn power(codeword: usize) -> usize {
         const POWERS: &'static [usize] = &[
-            5,
-            4,
-            10,
-            3,
-            15,
-            9,
-            29,
-            2,
-            34,
-            14,
-            50,
-            8,
-            37,
-            28,
-            20,
-            1,
-            25,
-            33,
-            46,
-            13,
-            53,
-            49,
-            42,
-            7,
-            17,
-            36,
-            39,
-            27,
-            55,
-            19,
-            57,
             0,
-            62,
-            24,
-            61,
-            32,
-            23,
-            45,
-            60,
-            12,
-            31,
-            52,
-            22,
-            48,
-            44,
-            41,
-            59,
+            1,
             6,
-            11,
-            16,
-            30,
-            35,
-            51,
-            38,
-            21,
+            2,
+            12,
+            7,
             26,
-            47,
-            54,
-            43,
+            3,
+            32,
+            13,
+            35,
+            8,
+            48,
+            27,
             18,
-            40,
+            4,
+            24,
+            33,
+            16,
+            14,
+            52,
+            36,
+            54,
+            9,
+            45,
+            49,
+            38,
+            28,
+            41,
+            19,
             56,
+            5,
+            62,
+            25,
+            11,
+            34,
+            31,
+            17,
+            47,
+            15,
+            23,
+            53,
+            51,
+            37,
+            44,
+            55,
+            40,
+            10,
+            61,
+            46,
+            30,
+            50,
+            22,
+            39,
+            43,
+            29,
+            60,
+            42,
+            21,
+            20,
+            59,
+            57,
             58,
         ];
 
@@ -273,25 +273,6 @@ impl<F: GaloisField> std::cmp::Eq for Codeword<F> {}
 impl<F: GaloisField> std::cmp::PartialEq<u8> for Codeword<F> {
     fn eq(&self, other: &u8) -> bool {
         self.bits == *other
-    }
-}
-
-impl<F: GaloisField> std::cmp::PartialOrd for Codeword<F> {
-    fn partial_cmp(&self, rhs: &Self) -> Option<std::cmp::Ordering> {
-        use std::cmp::Ordering::*;
-
-        match (self.power(), rhs.power()) {
-            (Some(p), Some(q)) => Some(p.cmp(&q)),
-            (Some(_), None) => Some(Greater),
-            (None, Some(_)) => Some(Less),
-            (None, None) => Some(Equal),
-        }
-    }
-}
-
-impl<F: GaloisField> std::cmp::Ord for Codeword<F> {
-    fn cmp(&self, rhs: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(rhs).unwrap()
     }
 }
 
@@ -509,9 +490,9 @@ mod test {
 
     #[test]
     fn test_for_power() {
-        assert_eq!(P25Codeword::for_power(0), 0b100000);
+        assert_eq!(P25Codeword::for_power(0), 0b000001);
         assert_eq!(P25Codeword::for_power(62), 0b100001);
-        assert_eq!(P25Codeword::for_power(63), 0b100000);
+        assert_eq!(P25Codeword::for_power(63), 0b000001);
     }
 
     #[test]
@@ -526,31 +507,28 @@ mod test {
 
     #[test]
     fn test_mul() {
-        assert_eq!((P25Codeword::new(0b011000) * P25Codeword::new(0b101000)), 0b011110);
-        assert_eq!((P25Codeword::new(0b000000) * P25Codeword::new(0b101000)), 0b000000);
-        assert_eq!((P25Codeword::new(0b011000) * P25Codeword::new(0b000000)), 0b000000);
+        assert_eq!((P25Codeword::new(0b000110) * P25Codeword::new(0b000101)), 0b011110);
+        assert_eq!((P25Codeword::new(0b000000) * P25Codeword::new(0b000101)), 0b000000);
+        assert_eq!((P25Codeword::new(0b000110) * P25Codeword::new(0b000000)), 0b000000);
         assert_eq!((P25Codeword::new(0b000000) * P25Codeword::new(0b000000)), 0b000000);
-        assert_eq!((P25Codeword::new(0b100001) * P25Codeword::new(0b100000)), 0b100001);
-        assert_eq!((P25Codeword::new(0b100001) * P25Codeword::new(0b010000)), 0b100000);
-        assert_eq!((P25Codeword::new(0b110011) * P25Codeword::new(0b110011)), 0b100111);
-        assert_eq!((P25Codeword::new(0b111101) * P25Codeword::new(0b111101)), 0b011001);
+        assert_eq!((P25Codeword::new(0b100001) * P25Codeword::new(0b000001)), 0b100001);
+        assert_eq!((P25Codeword::new(0b100001) * P25Codeword::new(0b000010)), 0b000001);
+        assert_eq!((P25Codeword::new(0b110011) * P25Codeword::new(0b110011)), 0b111001);
+        assert_eq!((P25Codeword::new(0b101111) * P25Codeword::new(0b101111)), 0b100110);
     }
 
 
     #[test]
     fn test_div() {
-        assert_eq!((P25Codeword::new(0b000100) / P25Codeword::new(0b101000)), 0b111010);
+        assert_eq!((P25Codeword::new(0b001000) / P25Codeword::new(0b000101)), 0b010111);
         assert_eq!((P25Codeword::new(0b000000) / P25Codeword::new(0b101000)), 0b000000);
-        assert_eq!((P25Codeword::new(0b011110) / P25Codeword::new(0b100000)), 0b011110);
-        assert_eq!((P25Codeword::new(0b011110) / P25Codeword::new(0b011110)), 0b100000);
+        assert_eq!((P25Codeword::new(0b011110) / P25Codeword::new(0b000001)), 0b011110);
+        assert_eq!((P25Codeword::new(0b011110) / P25Codeword::new(0b011110)), 0b000001);
     }
 
     #[test]
     fn test_cmp() {
-        assert!(P25Codeword::new(0b100000) > P25Codeword::new(0b000000));
         assert!(P25Codeword::new(0b000000) == P25Codeword::new(0b000000));
-        assert!(P25Codeword::new(0b010000) > P25Codeword::new(0b100000));
-        assert!(P25Codeword::new(0b100001) > P25Codeword::new(0b100000));
     }
 
     #[test]
@@ -566,10 +544,10 @@ mod test {
         let p = TestPolynomial::new((0..3).map(|_| {
             P25Codeword::for_power(0)
         }));
-        assert_eq!(p.eval(P25Codeword::for_power(1)), 0b111000);
+        assert_eq!(p.eval(P25Codeword::for_power(1)), 0b000111);
 
         let p = p.shift();
-        assert_eq!(p.eval(P25Codeword::for_power(1)), 0b110000);
+        assert_eq!(p.eval(P25Codeword::for_power(1)), 0b000011);
     }
 
     #[test]
