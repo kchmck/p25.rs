c1e753782af68cc06fc3e4c03ab9e3c0c5ef2bf5
Mick Koch <mick@kochm.co>
2016-12-02 11:57:11 -0500

    Make all voice modules public

--- src/voice/mod.rs
+++ src/voice/mod.rs
@@ -1,11 +1,10 @@
 //! Receive and decode voice-related data units.
 
-mod descramble;
-mod rand;
-
 pub mod control;
 pub mod crypto;
+pub mod descramble;
 pub mod frame;
 pub mod frame_group;
 pub mod header;
+pub mod rand;
 pub mod term;
